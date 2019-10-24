3f4ee126fc35355809ceaefe79532aa8a528a73f
sepehr ansaripour <pehrans@gmail.com>
2019-10-23 23:38:42 -0400

    Add, remove, update dependencies

--- project.clj
+++ project.clj
@@ -2,15 +2,27 @@
   :plugins [[lein-cljsbuild "1.1.7"]
             [lein-tools-deps "0.4.5"]
             [lein-ring "0.12.5"]]
-  :lein-tools-deps/config {:config-files [:install :user :project]}
-  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.520"]
+                 [com.bhauman/figwheel-main "0.2.3"]
+                 [com.bhauman/rebel-readline-cljs "0.1.4"]
+                 [reagent "0.8.1"]
+                 [re-frame "0.10.8"]
+                 [clj-commons/secretary "1.2.4"]
+                 [compojure "1.6.1"]
+                 [yogthos/config "1.1.5"]
+                 [ring "1.7.1"]
+                 [devcards "0.2.6"]
+                 [binaryage/devtools "0.9.10"]
+                 [net.dhleong/spade "1.0.3"]
+                 [org.clojure/test.check "0.9.0"]]
   :min-lein-version "2.5.3"
   :source-paths ["src/clj" "src/cljs"]
   :test-paths ["test/cljs"]
   :clean-targets ^{:protect false} ["resources/public/cljs-out" "resources/public/js/compiled" "target"]
-  :aliases {"prod"      ["with-profile" "prod" "cljsbuild" "once" "min"]
-            "fig"       ["trampoline" "run" "-m" "figwheel.main"]
-            "fig:build" ["trampoline" "run" "-m" "figwheel.main" "-b" "dev" "-r"]}
+  :aliases {"prod"    ["with-profile" "prod" "cljsbuild" "once" "min"]
+            "fig"     ["trampoline" "run" "-m" "figwheel.main"]
+            "fig:dev" ["trampoline" "run" "-m" "figwheel.main" "-b" "dev" "-r"]}
 
   :profiles
   {:dev
@@ -23,13 +35,13 @@
                             [javax.servlet/servlet-api "2.5"]
                             [ring/ring-mock "0.3.2"]]}
    :prod    {:dependencies [[day8.re-frame/tracing-stubs "0.5.3"]]}
-   :uberjar {:source-paths ["env/prod/clj"]
-             :dependencies [[day8.re-frame/tracing-stubs "0.5.3"]]
-             :omit-source  true
-             :main         my-website.server
-             :aot          [my-website.server]
-             :uberjar-name "my-website.jar"
-             :prep-tasks   ["compile" ["prod"]]
+   :uberjar {:source-paths       ["env/prod/clj"]
+             :dependencies       [[day8.re-frame/tracing-stubs "0.5.3"]]
+             :omit-source        true
+             :main               my-website.server
+             :aot                [my-website.server]
+             :uberjar-name       "my-website.jar"
+             :prep-tasks         ["compile" ["prod"]]
              :uberjar-exclusions [#"devcards\.html"]}}
 
   :ring {:handler my-website.handler/app}
