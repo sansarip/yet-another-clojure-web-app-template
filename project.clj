8905d1cf435fb8f12f919e0976eda53674bb4c0f
sepehr ansaripour <pehrans@gmail.com>
2019-12-08 18:20:44 -0500

    Add prod uberjar alias

--- project.clj
+++ project.clj
@@ -20,20 +20,19 @@
   :source-paths ["src/clj" "src/cljs"]
   :test-paths ["test/cljs"]
   :clean-targets ^{:protect false} ["resources/public/cljs-out" "resources/public/js/compiled" "target"]
-  :aliases {"prod"     ["with-profile" "prod" "cljsbuild" "once" "min"]
+  :aliases {"prod"     ["with-profile" "uberjar" "ring" "uberjar"]
             "fig:prod" ["run" "-m" "figwheel.main" "-bo" "prod"]
             "fig:dev"  ["trampoline" "run" "-m" "figwheel.main" "-b" "dev" "-r"]}
 
   :profiles
-  {:dev
-            {:dependencies [[binaryage/devtools "0.9.10"]
-                            [day8.re-frame/re-frame-10x "0.4.2"]
-                            [day8.re-frame/tracing "0.5.3"]
-                            [com.bhauman/figwheel-main "0.2.3"]
-                            [com.bhauman/rebel-readline-cljs "0.1.4"]
-                            [devcards "0.2.5"]
-                            [javax.servlet/servlet-api "2.5"]
-                            [ring/ring-mock "0.3.2"]]}
+  {:dev {:dependencies [[binaryage/devtools "0.9.10"]
+                        [day8.re-frame/re-frame-10x "0.4.2"]
+                        [day8.re-frame/tracing "0.5.3"]
+                        [com.bhauman/figwheel-main "0.2.3"]
+                        [com.bhauman/rebel-readline-cljs "0.1.4"]
+                        [devcards "0.2.5"]
+                        [javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.2"]]}
    :prod    {:dependencies [[day8.re-frame/tracing-stubs "0.5.3"]]}
    :uberjar {:source-paths       ["env/prod/clj"]
              :dependencies       [[day8.re-frame/tracing-stubs "0.5.3"]]
@@ -44,15 +43,6 @@
              :prep-tasks         ["compile" ["fig:prod"]]
              :uberjar-exclusions [#"devcards\.html"]}}
 
-  :ring {:handler my-website.handler/app}
+  :ring {:handler my-website.handler/app})
 
-  :cljsbuild
-  {:builds
-   {:min
-    {:source-paths ["src/cljs"]
-     :compiler     {:main            my_website.core
-                    :output-to       "resources/public/js/compiled/app.js"
-                    :optimizations   :simple
-                    :closure-defines {goog.DEBUG false}
-                    :pretty-print    false}}}})
 
