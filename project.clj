5077d503e84a1ad5213584263df6038092d8806b
sepehr ansaripour <pehrans@gmail.com>
2019-12-10 22:14:09 -0500

    Update version, descrip, and url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject yacwat/lein-template "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject yacwat/lein-template "0.1.0"
+  :description "A Clojure web application template oasis for the weary full-stack wanderer"
+  :url "https://github.com/sansarip/yet-another-clojure-web-app-template"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :eval-in-leiningen true)
